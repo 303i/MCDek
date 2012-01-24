d4f19a01f6eddf4ba315a29e7f2550afd0cc02ac
303i <sinsofasolarempire@iinet.net.au>
2012-01-24 18:47:10 +0800

    Update MCDek/Properties/AssemblyInfo.cs

--- MCDek/Properties/AssemblyInfo.cs
+++ MCDek/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MCLawl")]
-[assembly: AssemblyDescription("A pretty epic Minecraft server software")]
+[assembly: AssemblyTitle("MCDek")]
+[assembly: AssemblyDescription("An Awesome Minecraft Server Software")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MCLawl")]
-[assembly: AssemblyCopyright("Copyright © 2010")]
+[assembly: AssemblyProduct("MCDek")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
